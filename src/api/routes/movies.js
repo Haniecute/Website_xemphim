8cd2393113925fac6079f18208b9f48aa6b27ab0
Vo Van Khanh <vovankhanh@khanh-3.local>
2023-06-28 01:27:11 +0700

    update api/movies and update .env

--- src/api/routes/movies.js
+++ src/api/routes/movies.js
@@ -89,16 +89,20 @@ router.get("/random", verify, async (req, res) => {
 });
 
 //GET ALL
-router.get("/", verify, async (req, res) => {
-  if (req.user.isAdmin) {
-    try {
-      const movies = await Movie.find();
-      res.status(200).json(movies.reverse());
-    } catch (err) {
-      res.status(500).json(err);
+outer.get("/", verify, async (req, res) => {
+  try {
+    const { search } = req.query;
+    let movies;
+
+    if (search !== "") {
+      movies = await Movie.find({ title: { $regex: search } });
+    } else {
+      movies = await Movie.find();
     }
-  } else {
-    res.status(403).json("You are not allowed!");
+
+    res.status(200).json(movies.reverse());
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
